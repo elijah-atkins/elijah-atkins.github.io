25ca6ab222c61efdd5d87e6d9c96b13e637161a3
elijah-atkins <elijahorama@gmail.com>
2020-05-01 12:03:15 -0600

    making navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 const Navbar = () => {
     return (
     <div className="navbar">
-        <p>Hello World</p>
+        <ul className="navbar-nav">
+            <li class="nav-item">
+                <a href="#" className="nav-link">
+                    Projects
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" className="nav-link">
+                    skills
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" className="nav-link">
+                    contact
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" className="nav-link">
+                    theme
+                </a>
+            </li>
+        </ul>
     </div>
     )
 }
