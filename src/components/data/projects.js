fd89ece9bff9ea840029af791a3ce6ffca1186d2
Elijah Atkins <elijahorama@gmail.com>
2020-05-05 21:25:06 -0600

    added svg files and styled projects page

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,6 +1,6 @@
 const projects = [
     {
-        id: "20200402",
+        id: "20200412",
         star: true,
         sprint: false,
         title: "NASA Astronomy Picture of the Day",
@@ -9,7 +9,25 @@ const projects = [
         description: "Single page web application made with React pulling data from NASA apod api with post as far back as 1995"
     },
     {
-        id: "20200410",
+        id: "20200316",
+        star: false,
+        sprint: false, 
+        title: "Paddle Ball",
+        url: "http://elijahatkins.com/Paddle-Ball-Game/paddle-ball.html",
+        git: "https://github.com/elijah-atkins/Paddle-Ball-Game",
+        description: "Brick breaker style game made in javascript"
+    },
+    {
+    id: "20200317",
+    star: false,
+    sprint: false, 
+    title: "Racing Game Demo",
+    url: "http://elijahatkins.com/Paddle-Ball-Game/racing-game.html",
+    git: "https://github.com/elijah-atkins/Paddle-Ball-Game",
+    description: "Simple racing game demo made from Paddle-ball game made in javascript"
+},
+    {
+        id: "20200415",
         star: false,
         sprint: true,
         title: "Rick and Morty Characters",
