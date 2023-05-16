9235acf1297d170cb1862c9d0a650989a68a5de9
Elijah Atkins <elijahorama@gmail.com>
2023-05-15 20:33:40 -0600

    added more characters

--- src/components/characters.js
+++ src/components/characters.js
@@ -2,6 +2,8 @@ import apollo from "../img/characters/apollo-fuzzmoon.png"
 import dolly from "../img/characters/dolly-dreadful.png"
 import mushy from "../img/characters/mushylp.png"
 import geebee from "../img/characters/geebee.png"
+import zed from "../img/characters/zed.png"
+import frox from "../img/characters/frox.png"
 
 const getRand = (n) => {
   
@@ -39,6 +41,22 @@ const characters = [
     title: "Mushy",
     description:
       "Mushy is a 3D character model designed for real time animation. It can be used as a presenter for educational videos on various topics. It is compatible with VSeeFace and VRChat, two popular applications for virtual reality. It was created and rigged using Blender, a free and open source software for 3D creation.",
+  },
+  {
+    id: "5",
+    rot: getRand(3),
+    img: zed,
+    title: "Zed",
+    description:
+      "Zed is a full body character that is drawn and animated using Moho Animation Software, a powerful 2D animation program. Zed is made of vector shapes that can be scaled and transformed without losing quality. Zed has a bone structure that allows him to move and deform realistically. Zed also has smart bone actions and smart warp mesh that enable him to change his facial expressions and head orientation.",
+  },
+  {
+    id: "6",
+    rot: getRand(3),
+    img: frox,
+    title: "Frox",
+    description:
+      "Frox is a vector-based character created and animated in Moho, a 2D animation software. She can resize and reshape her vector parts without losing quality. She can also move and bend her body parts with bones. She can make different faces and head turns with smart bones and smart warp.",
   },
 ]
 export default characters
